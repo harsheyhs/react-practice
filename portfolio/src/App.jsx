10306362c2ea1c3486a6691203cd17c9b5db82d2
harsheyhs <109295677+harsheyhs@users.noreply.github.com>
2023-10-06 03:33:41 +0530

    added portfolio landing pg

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './app.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main>
+        <article>
+          <h1>Hi! I'm Harshita.</h1>
+          <p>I'm an aspiring front-end web developer.<br></br>
+            You can check out my projects <a href="./projects.html">here.</a>
+          </p>
+        </article>
+        <img src="./imgs.png" alt="a woman holding a cat" />
+
+      </main>
+    </>
+  )
+}
+
+export default App
