a8e93ea492de192841a8e6b0cac671db300ee24c
harsheyhs <109295677+harsheyhs@users.noreply.github.com>
2023-10-06 03:42:18 +0530

    removed projects.html

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <article>
           <h1>Hi! I'm Harshita.</h1>
           <p>I'm an aspiring front-end web developer.<br></br>
-            You can check out my projects <a href="./projects.html">here.</a>
+            You can check out my projects <a href="#">here.</a>
           </p>
         </article>
         <img src="./imgs.png" alt="a woman holding a cat" />
